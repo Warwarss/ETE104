99f5230fb624474852c8ce6d0e9d89e814e4fba5
Warwarss <96444036+Warwarss@users.noreply.github.com>
2022-03-17 12:09:17 +0300

    Update ETE104 2.2.asm

--- ETE104 2.2.asm
+++ ETE104 2.2.asm
@@ -36,16 +36,18 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
             mov    #1,4(R10)
 
 
-Loop        push   6(R10)
+Loop
+            push   6(R10)
             add    2(R10),6(R10)
-            adc    8(R10)
             mov    6(R10),10(R10)
             pop    6(R10)
 
-            push   8(R10)
-            add    4(R10),8(R10)
-            mov    8(R10),12(R10)
-            pop    8(R10)
+            push   4(R10)
+            add    0(R10),4(R10)
+            adc    10(R10)
+            mov    4(R10),8(R10)
+            pop    4(R10)
+
             add    #0x0004,R10
             jmp    Loop
 
