454af7b13a9c943b27c49c2dae70b1360a20baea
Warwarss <96444036+Warwarss@users.noreply.github.com>
2022-03-17 10:49:36 +0300

    Create ETE104 2.2.asm

---
+++ ETE104 2.2.asm
@@ -0,0 +1,60 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+            mov    #0x2400,R10
+            mov    #1,R9
+            mov    #0,R8
+            mov    R9,0(R10)
+            add    #0x0002,R10
+
+Loop        push   R9
+            add    R8,R9
+            pop    R8
+            mov    R9,0(R10)
+            add    #0x0002,R10
+            jmp    Loop
+
+
+
+
+
+
+
+
+
+
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
