790a202915f82de1a1cf72c3166cbe01bd49740b
Warwarss <96444036+Warwarss@users.noreply.github.com>
2022-03-22 20:54:57 +0300

    Update ETE104 2.2.asm

--- ETE104 2.2.asm
+++ ETE104 2.2.asm
@@ -23,29 +23,29 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-            mov    #0x2400,R10
+            mov    #0x2400,R10     ;Pointeri ayarlıyorum
 
-            mov    #0,2(R10)
-            mov    #1,0(R10)
+            mov    #0,2(R10)       ;İlk sayıyı sıfırlıyorum
+            mov    #0,0(R10)
 
-            mov    #0,6(R10)
+            mov    #0,6(R10)       ;İkinci sıradakı sayıyı 1 yapıyorum
             mov    #1,4(R10)
 
 
 Loop
-            push   6(R10)
-            add    2(R10),6(R10)
-            mov    6(R10),10(R10)
-            pop    6(R10)
+            push   6(R10)           ;İkinci sayının büyük kısmını Stack'e yazıyorum
+            add    2(R10),6(R10)    ;Birinci sayının büyük kısmı ile ikinci sayının büyük kısmı toplanıyor
+            mov    6(R10),10(R10)   ;Ve sonuç üçüncü sayının büyük kısmına yazılıyor
+            pop    6(R10)           ;İkinci sayının büyük kısmını geri yazıyorum
 
-            push   4(R10)
-            add    0(R10),4(R10)
-            adc    10(R10)
-            mov    4(R10),8(R10)
-            pop    4(R10)
+            push   4(R10)           ;İkinci sayının küçük kısmını Stack'e yazıyorum
+            add    0(R10),4(R10)    ;Birinci sayıyla ikinci sayının küçük kısmı toplanıyor
+            adc    10(R10)          ;Eğer carry bit varsa üçüncü sayının büyük kısmına ekleniyor
+            mov    4(R10),8(R10)    ;Toplam, üçüncü sayının küçük kısmına yazılıyor
+            pop    4(R10)           ;Stack'e kaydettiğim sayıyı geri yazıyorum
 
-            add    #0x0004,R10
-            jmp    Loop
+            add    #0x0004,R10      ;Pointeri 4 arttırıyorum
+            jmp    Loop             ;Loop
 
 
 
